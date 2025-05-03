e6c72190eb53f6fc7aa1c4f1320354747be8ac6f
Stepan Leonov <stepan.leonov777@gmail.com>
2025-05-03 18:45:17 +0300

    add tailwind / daisy ui / icons / dark mode support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
 });
