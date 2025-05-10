d165943668934a2a81125c72adfcea6c38e08561
Stepan Leonov <stepan.leonov777@gmail.com>
2025-05-10 22:38:33 +0300

    added color theme toggle (#51)

--- components/app/nav-bar.vue
+++ components/app/nav-bar.vue
@@ -6,6 +6,7 @@
       </NuxtLink>
     </div>
     <div class="navbar-end">
+      <AppThemeToggle />
       <button class="btn btn-accent">
         Sign in with GitHub
         <Icon name="tabler:brand-github" size="20" />
