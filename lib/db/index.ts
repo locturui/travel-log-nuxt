1a5239f2f1c010e5ee6ed5b7318cb5c6048fce32
Stepan Leonov <stepan.leonov777@gmail.com>
2025-05-10 23:53:21 +0300

    setup db

---
+++ lib/db/index.ts
@@ -0,0 +1,11 @@
+import { drizzle } from "drizzle-orm/libsql";
+
+import * as schema from "~/lib/db/schema";
+import env from "~/lib/env";
+
+const db = drizzle({ connection: {
+  url: env.TURSO_DATABASE_URL,
+  authToken: env.NODE_ENV === "development" ? undefined : env.TURSO_AUTH_TOKEN,
+}, casing: "snake_case", schema });
+
+export default db;
