8b86f20b8fee3336b8704869dcb0c054ca635295
Stepan Leonov <stepan.leonov777@gmail.com>
2025-05-10 22:02:48 +0300

    added landing page

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        LogIt
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <button class="btn btn-accent">
+        Sign in with GitHub
+        <Icon name="tabler:brand-github" size="20" />
+      </button>
+    </div>
+  </div>
+</template>
