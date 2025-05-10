1d83c41ebe9cce3e39ab7d5078d1386656b818a2
Stepan Leonov <stepan.leonov777@gmail.com>
2025-05-10 22:07:42 +0300

    Tl 11 (#50)
    * add tailwind / daisy ui / icons / dark mode support
    
    * added landing page

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-5">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          LogIt
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and adventures. Add locations, photos, and notes to create a personal travel log.
+        </p>
+        <button class="btn btn-accent">
+          Sign in with GitHub
+          <Icon name="tabler:brand-github" size="20" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
