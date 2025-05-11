af4ca7e8eda429055ad49f6472453f264cd563ed
Stepan Leonov <stepan.leonov777@gmail.com>
2025-05-11 05:00:50 +0300

    added user session representation / logout logic (#56)

---
+++ pages/sign-out.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+onMounted(() => {
+  const authStore = useAuthStore();
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="card bg-base-300 mx-auto container mt-5 min-h-72 text-center flex flex-col justify-center items-center gap-4">
+    <span class="loading loading-dots loading-xl" />
+  </div>
+</template>
+
+<style>
+
+</style>
