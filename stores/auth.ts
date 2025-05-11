fa0074230f8427361baab6f7208e4088e4044598
Stepan Leonov <stepan.leonov777@gmail.com>
2025-05-11 05:38:15 +0300

    ssr session handling (#58)

--- stores/auth.ts
+++ stores/auth.ts
@@ -4,11 +4,16 @@ import { defineStore } from "pinia";
 const authClient = createAuthClient();
 
 export const useAuthStore = defineStore("useAuthStore", () => {
-  const session = authClient.useSession();
-  const user = computed(() => session.value.data?.user);
+  const session = ref <Awaited<ReturnType<typeof authClient.useSession>> | null> (null);
+  async function init() {
+    const data = await authClient.useSession(useFetch);
+    session.value = data;
+  }
+
+  const user = computed(() => session.value?.data?.user);
 
   const loading = computed(() => {
-    return session.value.isPending || session.value.isRefetching;
+    return session.value?.isPending;
   });
 
   async function signIn() {
@@ -29,5 +34,6 @@ export const useAuthStore = defineStore("useAuthStore", () => {
     signIn,
     signOut,
     user,
+    init,
   };
 });
