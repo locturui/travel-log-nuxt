0c6b97956710e05bab464e3335f142f5c13b943f
Stepan Leonov <stepan.leonov777@gmail.com>
2025-05-15 06:39:00 +0300

    sidebar location display (#65)

---
+++ stores/sidebar.ts
@@ -0,0 +1,18 @@
+import { defineStore } from "pinia";
+
+export type SidebarItem = {
+  id: string;
+  label: string;
+  icon: string;
+  href: string;
+};
+
+export const useSidebarStore = defineStore("useSidebarStore", () => {
+  const sidebarItems = ref<SidebarItem[]>([]);
+  const loading = ref(false);
+
+  return {
+    sidebarItems,
+    loading,
+  };
+});
